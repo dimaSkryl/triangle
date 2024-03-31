a00e9e900ac041a23d0d55cdca20200a00ede0a7
dimaSkryl <125749611+dimaSkryl@users.noreply.github.com>
2024-03-31 18:18:36 +0300

    Add files via upload

---
+++ triangle.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+void drawTriangle(int n) {
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= n - i; ++j) {
+            cout << " ";
+        }
+        for (int k = 1; k <= 2 * i - 1; ++k) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    int n;
+    cout << "Введите высоту треугольника: ";
+    cin >> n;
+
+    drawTriangle(n);
+}
